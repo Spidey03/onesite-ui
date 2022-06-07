ceca3015f0bac5df30c4c345733f0eae357e04ba
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-07 23:09:33 +0530

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './header.css';
+import header_img from '../../assets/home.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='onesite__header' id="home">
+      <div className='onesite__header-content'>
+        <h1>Easy way to find a <br />perfect property</h1>
+        <p>We provide a complete service for the sale, <br />purchase or rental of real estate.</p>
+      </div>
+      <div className='onesite__header-image'>
+        <img src={header_img} alt='header-img' />
+      </div>
+    </div>
   )
 }
 
