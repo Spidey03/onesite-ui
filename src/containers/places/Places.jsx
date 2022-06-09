e8f2d6dfaf159ba645c59188e4b7148c081d346b
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-09 21:22:15 +0530

    fix ui header & places

--- src/containers/places/Places.jsx
+++ src/containers/places/Places.jsx
@@ -6,18 +6,22 @@ import america from './../../assets/america.png'
 import london from './../../assets/london.png'
 import france from './../../assets/france.png'
 import spain from './../../assets/spain.png'
+import india from './../../assets/india.jpg'
+import sydney from './../../assets/sydney.jpg'
 
 const Places = () => {
   return (
     <div className='onesite__places'>
-      <div className='onesite__place-header'>
-        <h1>We are available in many well known countries</h1>
+      <div className='onesite__places-header'>
+        <h1>We are available in many well-known countries</h1>
       </div>
       <div className='onesite__places-container'>
-        <Place imgURL={america} country='America'/>
-        <Place imgURL={spain} country='Spain'/>
-        <Place imgURL={london} country='london'/>
-        <Place imgURL={france} country='france'/>
+        <Place imgURL={america} country="america" />
+        <Place imgURL={france} country="france" />
+        <Place imgURL={india} country="india" />
+        <Place imgURL={sydney} country="sydney" />
+        <Place imgURL={london} country="london" />
+        <Place imgURL={spain} country="spain" />
       </div>
     </div>
   )
