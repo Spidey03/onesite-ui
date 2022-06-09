99d809d4d752c9d7e5bdcf87e4485c5147268d57
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-10 00:19:08 +0530

    Project card component

--- src/components/project_card/ProjectCard.jsx
+++ src/components/project_card/ProjectCard.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './project_card.css';
 
-const ProjectCard = () => {
+const ProjectCard = ({projectInfo}) => {
   return (
-    <div>ProjectCard</div>
+    <div className='onesite__project_card'>
+      <div className='onesite__project_card-img'>
+        <img src={projectInfo.imgURL} alt='project' />
+      </div>
+      <div className='onesite__project_card-details'>
+        <h3>{projectInfo.street}, {projectInfo.village}</h3>
+        <div className='onesite__project_card-details__features'>
+          <p>{projectInfo.feature1}</p>
+          <p>{projectInfo.feature2}</p>
+          <p>{projectInfo.feature3}</p>
+          <p>Posted by {projectInfo.owner_name}</p>
+          <p className='onesite__project_card-details__features-price'>{projectInfo.price}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
