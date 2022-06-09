83193992802d2f47169554f1195dde36a2bb0d27
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-10 00:19:16 +0530

    Projects

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import ProjectCard from './../../components/project_card/ProjectCard';
 import './projects.css';
+import india from './../../assets/india.jpg'
+
+const projectInfo = {
+  'street': '103/143 West Street',
+  'imgURL': india,
+  'village': 'Crows Nest',
+  'feature1': '10 Bedroom(s)',
+  'feature2': '150 M',
+  'feature3': '2 Garage',
+  'owner_name': 'Peter Parker',
+  'price': '$45, 545'
+}
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='onesite__projects' id="properties">
+      <div className='onesite__projects-header'>
+        <h1>Recently Added</h1>
+        <p>See all</p>
+      </div>
+      <div className='onesite__projects-container'>      
+        <ProjectCard projectInfo={projectInfo}/>
+        <ProjectCard projectInfo={projectInfo}/>
+        <ProjectCard projectInfo={projectInfo}/>
+        <ProjectCard projectInfo={projectInfo}/>
+        <ProjectCard projectInfo={projectInfo}/>
+      </div>
+    </div>
   )
 }
 
