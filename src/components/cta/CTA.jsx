ec83f2ab297342b3b2d2a13f8ca623b6c6d2346d
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-22 23:18:38 +0530

    cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './cta.css';
+import cta from '../../assets/cta.png';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='onesite__cta'>
+      <div className='onesite__cta-content'>
+        <h1>Find your best Real Estate</h1>
+        <p>We provide a complete service for the sale, purchase or rental of real estate</p>
+        <button type='button'>Contact Us</button>
+      </div>
+      <div className='onesite__cta-image'>
+        <img src={cta} alt="cta" />
+      </div>
+    </div>
   )
 }
 
