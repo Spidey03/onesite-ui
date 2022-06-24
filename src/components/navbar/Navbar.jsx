bdb31dd6c636693958ca207d2422330b4c23c5f2
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-24 22:08:06 +0530

    new logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/1site.png'; 
 
 const Menu = () => (
   <>
