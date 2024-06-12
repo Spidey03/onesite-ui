ede835509c644be094e233bdb8053a7a6562a789
Spidey03 <sayhai2naveenkumar@gmail.com>
2024-06-12 20:40:24 +0530

    Fetch Projects & Show
    - Use get_sites API to fetch projects
    - Use Access Token from local storage

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import ProjectCard from './../../components/project_card/ProjectCard';
 import './projects.css';
 import india from './../../assets/india.jpg'
 import axios from 'axios';
+
+// TODO: Update the access token
+localStorage.setItem('accessToken', "zRW6L3Yfzns2UbBuhVCal9gIecv2QG");
 
 let projectInfo = {
   'street': '103/143 West Street',
@@ -16,14 +19,63 @@ let projectInfo = {
 }
 
 const Projects = () => {
-  console.log(get_proejcts());
-  let projectCards = <>
-    <ProjectCard projectInfo={projectInfo}/>
-    <ProjectCard projectInfo={projectInfo}/>
-    <ProjectCard projectInfo={projectInfo}/>
-    <ProjectCard projectInfo={projectInfo}/>
-    <ProjectCard projectInfo={projectInfo}/>
-  </>
+  const [projectCards, setProjectCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function get_projects() {
+      console.log("Base URL of Backend: ", process.env.REACT_APP_ONESITE_BASEURL);
+      const accessToken = localStorage.getItem('accessToken');
+      var config = {
+        method: 'get',
+        url: process.env.REACT_APP_ONESITE_BASEURL + '/onesite/get_sites',
+        headers: { 
+          'Authorization': `Bearer ${accessToken}`
+        }
+      };
+      await axios(config)
+      .then(function (response) {
+        if (response.status === 200) {
+          let responseJson = response.data;
+          responseJson.forEach(siteDetails => {
+            projectInfo = {
+              "id": siteDetails.id,
+              'street': siteDetails.street_name,
+              'imgURL': india,
+              'village': siteDetails.village,
+              'feature1': siteDetails.ft1,
+              'feature2': siteDetails.ft2,
+              'feature3': siteDetails.ft3,
+              'owner_name': siteDetails.owner.first_name,
+              'price': siteDetails.price
+            }
+            projectCards.push(<ProjectCard projectInfo={projectInfo}/>);
+          });
+          setProjectCards(projectCards);
+          setIsLoading(false);
+        } else {
+          console.log("Got ", response.status, "while calling Get Sites API");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+    get_projects();
+  }, []);
+  if (isLoading) {
+    (
+      <div className='onesite__projects' id="properties">
+        <div className='onesite__projects-header'>
+          <h1>Recently Added</h1>
+          <p>See all</p>
+        </div>
+        <div className='onesite__projects-container'>
+          <p>Still Loading....</p>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className='onesite__projects' id="properties">
       <div className='onesite__projects-header'>
@@ -37,47 +89,4 @@ const Projects = () => {
   )
 }
 
-function get_proejcts() {
-  console.log("Base URL of Backend: ", process.env.REACT_APP_ONESITE_BASEURL);
-
-  
-  var config = {
-    method: 'get',
-    url: process.env.REACT_APP_ONESITE_BASEURL + '/onesite/get_sites',
-    headers: { 
-      'Authorization': 'Bearer tTULiD0VEw1eF92u5HTZdklp09UbHN'
-    }
-  };
-  console.log(config);
-
-  axios(config)
-  .then(function (response) {
-    if (response.status === 200) {
-      let responseJson = response.data;
-      console.log(responseJson);
-      for(let item in responseJson){
-        console.log(item);
-      }
-      {responseJson.values().map(function(siteDetails, i){
-        projectInfo = {
-          'street': siteDetails.street,
-          'imgURL': india,
-          'village': siteDetails.village,
-          'feature1': siteDetails.ft1,
-          'feature2': siteDetails.ft2,
-          'feature3': siteDetails.ft3,
-          'owner_name': siteDetails.owner.first_name,
-          'price': siteDetails.price
-        }
-        console.log(projectInfo);
-        return <ProjectCard projectInfo={projectInfo}/>;
-      })}
-    } else {
-      console.log("Got ", response.status, "while calling Get Sites API");
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-};
 export default Projects
